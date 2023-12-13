1b96efff04fb39136ceed2a48ab4f074acd77711
Chrystel21 <153571689+Chrystel21@users.noreply.github.com>
2023-12-13 10:51:45 +0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -2,33 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ContactTable from './ContactTable';
 import ContactForm from './ContactForm';
+import ViewContact from './ViewContact';
 import UpdateContact from './UpdateContact';
 import DeleteContact from './DeleteContact';
-import ViewContact from './ViewContact';
-import './styles.css'; // Make sure to import your global styles here
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/add" exact>
-          <ContactForm />
-        </Route>
-        <Route path="/view/:id" exact>
-          <ViewContact />
-        </Route>
-        <Route path="/update/:id" exact>
-          <UpdateContact />
-        </Route>
-        <Route path="/delete/:id" exact>
-          <DeleteContact />
-        </Route>
-        <Route path="/" exact>
-          <ContactTable />
-        </Route>
+        <Route path="/" exact component={ContactTable} />
+        <Route path="/add" component={ContactForm} />
+        <Route path="/view/:id" component={ViewContact} />
+        <Route path="/update/:id" component={UpdateContact} />
+        <Route path="/delete/:id" component={DeleteContact} />
       </Switch>
     </Router>
   );
-};
+}
 
 export default App;
