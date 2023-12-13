28358fc865ed728bffd9f9444b8beaca626eff9e
Chrystel21 <153571689+Chrystel21@users.noreply.github.com>
2023-12-13 10:16:05 +0800

    Update deleteContact.jsx

--- deleteContact.jsx
+++ deleteContact.jsx
@@ -1,79 +1,31 @@
-// DeleteModal.jsx
 import React from 'react';
-import {
-  Modal,
-  Typography,
-  Button,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-} from '@mui/material';
-import './styles.css';
+import { useHistory } from 'react-router-dom';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 
-const DeleteModal = ({ contact, isOpen, onClose, onDelete, id }) => {
+const DeleteContact = ({ contact, isOpen, onClose, onDelete, id }) => {
+  const history = useHistory();
+
   const handleConfirmDelete = () => {
-    onDelete();
+    onDelete(contact);
     onClose();
+    // Optionally, you can redirect to the main page after deletion
+    history.push('/');
   };
 
   return (
-    <Modal open={isOpen} onClose={onClose}>
-      <div className='modal-container'>
-        <Typography variant='h6'> Confirm Deletion </Typography>
-        <Typography>Are you sure you want to delete this contact?</Typography>
-        <Typography>ID: {id}</Typography>
-        <Typography>Full Name: {contact?.fullName}</Typography>
-        <Typography>Email: {contact?.emailAddress}</Typography>
-        <Typography>Contact Number: {contact?.contactNumber}</Typography>
-        <Typography>Location: {contact?.location}</Typography>
-        <Typography>Registered Date: {contact?.registeredDate}</Typography>
-
-        {/* Confirmation Dialog */}
-        <Dialog open={isOpen} onClose={onClose}>
-          <DialogTitle>Confirm Deletion</DialogTitle>
-          <DialogContent>
-            <Typography>
-              Are you sure you want to delete the record?
-            </Typography>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={onClose} color="primary">
-              No
-            </Button>
-            <Button onClick={handleConfirmDelete} color="primary">
-              Yes
-            </Button>
-          </DialogActions>
-        </Dialog>
-        
+    <Dialog open={isOpen} onClose={onClose}>
+      <DialogTitle>Delete Contact</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Are you sure you want to delete the contact with ID {id} - {contact.fullName}?
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
         <Button onClick={onClose} color="primary">
           Cancel
         </Button>
         <Button onClick={handleConfirmDelete} color="primary">
-          Confirm Delete
-        </Button>
-      </div>
-    </Modal>
-  );
-};
-
-const DeleteContact = ({ contact, isOpen, onClose, onDelete, id }) => {
-  return (
-    <Dialog open={isOpen} onClose={onClose}>
-      <DialogTitle>Delete Contact Details</DialogTitle>
-      <DialogContent>
-        <DeleteModal
-          contact={contact}
-          isOpen={isOpen}
-          onClose={onClose}
-          onDelete={onDelete}
-          id={id}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose} color="primary">
-          Back
+          Delete
         </Button>
       </DialogActions>
     </Dialog>
