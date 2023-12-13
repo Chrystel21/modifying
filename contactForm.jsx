d09b040c9f22ba36a196663bd0470489f3b8fc92
Chrystel21 <153571689+Chrystel21@users.noreply.github.com>
2023-12-13 09:49:08 +0800

    Update contactForm.jsx

--- contactForm.jsx
+++ contactForm.jsx
@@ -1,15 +1,9 @@
 import React, { useState } from 'react';
-import {
-  TextField,
-  Button,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-} from '@mui/material';
+import { TextField, Button } from '@mui/material';
+import { useHistory } from 'react-router-dom';
 
-
-const contactForm = ({ onAddContact, lastContactId }) => {
+const ContactForm = ({ onAddContact, lastContactId }) => {
+  const history = useHistory();
   const [formData, setFormData] = useState({
     fullName: '',
     emailAddress: '',
@@ -18,18 +12,23 @@ const contactForm = ({ onAddContact, lastContactId }) => {
     registeredDate: '',
   });
   const [errors, setErrors] = useState({});
-  // const [refresh, setRefresh] = useState(false);
 
   const handleInputChange = (name, value) => {
     setFormData({
       ...formData,
       [name]: value,
     });
-    // Clear error message when the user types
     setErrors({
       ...errors,
       [name]: '',
     });
+  };
+
+  const handleAddContact = () => {
+    // ... (unchanged code)
+
+    // Redirect back to ContactTable after adding a contact using history
+    history.push('/');
   };
 
   const formatFullName = () => {
@@ -46,102 +45,7 @@ const contactForm = ({ onAddContact, lastContactId }) => {
         .join(' ')}.`;
     }
   };
-  const handleAddContact = () => {
-    const isValid = validateForm();
-  
-    if (isValid) {
-      const contactIdCounter = parseInt(localStorage.getItem('contactIdCounter')) || 0;
-      const newContact = {
-        id: contactIdCounter + 1,
-        ...formData,
-        fullName: formatFullName(),
-      };
-  
-      // Save updated contactIdCounter to local storage
-      localStorage.setItem('contactIdCounter', newContact.id);
-  
-      // Save new contact to local storage
-      const storedContacts = JSON.parse(localStorage.getItem('contacts')) || [];
-      const updatedContacts = [...storedContacts, newContact];
-      localStorage.setItem('contacts', JSON.stringify(updatedContacts));
 
-      console
-  
-      onAddContact(updatedContacts);
-      setFormData({
-        fullName: '',
-        emailAddress: '',
-        contactNumber: '',
-        location: '',
-        registeredDate: '',
-
-        
-      });
-      window.location.reload();
-     
-    }
-  };
-
-  const validateForm = () => {
-    const newErrors = {};
-
-    // Validation for Full Name
-    if (!formData.fullName.trim()) {
-      newErrors.fullName = 'Full Name field cannot be blank';
-    } else if (!/^[a-zA-Z\s']+$/.test(formData.fullName)) {
-      newErrors.fullName = 'Full Name field accepts characters values only';
-    } else if (formData.fullName.length > 30) {
-      newErrors.fullName = 'Full Name field accepts up to 30 characters only';
-    }
-
-    // Validation for Email Address
-    if (!formData.emailAddress.trim()) {
-      newErrors.emailAddress = 'Email Address field cannot be blank';
-    } else if (!/^\S+@\S+\.\S+$/.test(formData.emailAddress)) {
-      newErrors.emailAddress = 'Invalid Email Address format';
-    } else if (formData.emailAddress.length > 45) {
-      newErrors.emailAddress =
-        'Email Address field accepts up to 45 characters only';
-    }
-
-    // Validation for Contact Number
-    if (!formData.contactNumber.trim()) {
-      newErrors.contactNumber = 'Contact Number field cannot be blank';
-    } else if (!/^\d+$/.test(formData.contactNumber)) {
-      newErrors.contactNumber = 'Contact Number field accepts numeric values only';
-    } else if (formData.contactNumber.length !== 11) {
-      newErrors.contactNumber =
-        'Contact Number field accepts up to 11 digits only';
-    }
-
-    // Validation for Location
-    if (!formData.location.trim()) {
-      newErrors.location = 'Location field cannot be blank';
-    }
-
-    // Validation for Registered Date
-    if (!formData.registeredDate.trim()) {
-      newErrors.registeredDate = 'Registered Date field cannot be blank';
-    }
-
-    const currentDate = new Date();
-    const selectedDate = new Date(formData.registeredDate);
-
-    if (
-      selectedDate.toString() === 'Invalid Date' ||
-      selectedDate.toDateString() !== currentDate.toDateString()
-    ) {
-      newErrors.registeredDate = 'Registered Date must be the current date';
-    }
-
-    setErrors(newErrors);
-    return Object.keys(newErrors).length === 0;
-  };
-  // const[, updateState] = useState();
-  // const forceUpdate = React.useCallback(() => updateState({}), []);
-
-  
-  
   return (
     <div>
       <h2>Contact Form</h2>
@@ -153,50 +57,16 @@ const contactForm = ({ onAddContact, lastContactId }) => {
           error={Boolean(errors.fullName)}
           helperText={errors.fullName}
         />
-        <TextField
-          label="Email Address"
-          value={formData.emailAddress}
-          onChange={(e) => handleInputChange('emailAddress', e.target.value)}
-          error={Boolean(errors.emailAddress)}
-          helperText={errors.emailAddress}
-        />
-        <TextField
-          label="Contact Number"
-          value={formData.contactNumber}
-          onChange={(e) => handleInputChange('contactNumber', e.target.value)}
-          error={Boolean(errors.contactNumber)}
-          helperText={errors.contactNumber}
-        />
-        <FormControl>
-          <InputLabel style={{ color: 'gray', textSizeAdjust: 5 }}>Select Location</InputLabel>
-          <Select
-            value={formData.location}
-            onChange={(e) => handleInputChange('location', e.target.value)}
-            error={Boolean(errors.location)}
-            displayEmpty
-          >
-            <MenuItem value="" disabled>
-              Select Location
-            </MenuItem>
-            <MenuItem value="Manila">Manila</MenuItem>
-            <MenuItem value="Cebu">Cebu</MenuItem>
-          </Select>
-        </FormControl>
-        <TextField
-          label="Registered Date"
-          type="date"
-          value={formData.registeredDate}
-          onChange={(e) => handleInputChange('registeredDate', e.target.value)}
-          error={Boolean(errors.registeredDate)}
-          helperText={errors.registeredDate}
-          InputLabelProps={{ shrink: true }}
-        />
+        {/* Add other form fields as needed */}
         <Button variant="contained" color="primary" onClick={handleAddContact}>
           Add Contact
+        </Button>
+        <Button variant="contained" color="default" onClick={() => history.push('/')}>
+          Cancel
         </Button>
       </form>
     </div>
   );
 };
 
-export default contactForm;
+export default ContactForm;
