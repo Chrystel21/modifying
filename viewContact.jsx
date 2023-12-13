61660e08ba92421f51d610ddcca33191fffcc234
Chrystel21 <153571689+Chrystel21@users.noreply.github.com>
2023-12-13 10:30:55 +0800

    Update viewContact.jsx

--- viewContact.jsx
+++ viewContact.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import { useHistory } from 'react-router-dom';
+import { Button, Typography } from '@mui/material';
 
-const viewContact = ({ contact, onClose, open }) => {
+const ViewContact = ({ contact, id }) => {
+  const history = useHistory();
+
+  if (!contact) {
+    return null;
+  }
+
+  const handleGoBack = () => {
+    history.goBack();
+  };
+
   return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Contact Details</DialogTitle>
-      <DialogContent>
-        {/* Render contact details here */}
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose} color="primary">
-          Close
-        </Button>
-      </DialogActions>
-    </Dialog>
+    <div>
+      <Typography variant="h6"> Contact Details </Typography>
+      <Typography>ID: {id}</Typography>
+      <Typography>Full Name: {contact?.fullName}</Typography>
+      <Typography>Email: {contact?.emailAddress}</Typography>
+      <Typography>Contact Number: {contact?.contactNumber}</Typography>
+      <Typography>Location: {contact?.location}</Typography>
+      <Typography>Registered Date: {contact?.registeredDate}</Typography>
+
+      <Button onClick={handleGoBack} color="primary">
+        Go Back
+      </Button>
+    </div>
   );
 };
 
-export default viewContact;
+export default ViewContact;
