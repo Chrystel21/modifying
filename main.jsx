422b7fc97d0eb7f7c26cc55e6948697f3cc3727e
Chrystel21 <153571689+Chrystel21@users.noreply.github.com>
2023-12-13 13:23:37 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container, CssBaseline } from '@mui/material';
 import ContactForm from './ContactForm';
 import ContactTable from './ContactTable';
 
 const Main = () => {
+  const [contacts, setContacts] = useState([]);
+
+  const handleAddContact = (newContact) => {
+    setContacts([...contacts, newContact]);
+  };
+
   return (
-    <div className="container">
-      <div className="form-container">
-        <ContactForm />
+    <Container component="main" maxWidth="lg">
+      <CssBaseline />
+      <div className="container">
+        <div className="form-container">
+          <ContactForm onAddContact={handleAddContact} />
+        </div>
+        <div className="table-container">
+          <ContactTable contacts={contacts} />
+        </div>
       </div>
-      <div className="table-container">
-        <ContactTable />
-      </div>
-    </div>
+    </Container>
   );
 };
 
