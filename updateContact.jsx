4a64dae086c819fd2543f99378f4a63a5942ebcd
Chrystel21 <153571689+Chrystel21@users.noreply.github.com>
2023-12-13 10:25:08 +0800

    Update updateContact.jsx

--- updateContact.jsx
+++ updateContact.jsx
@@ -1,20 +1,21 @@
-// UpdateContact.jsx
 import React, { useState, useEffect } from 'react';
 import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
   Button,
   TextField,
   FormControl,
   InputLabel,
   Select,
   MenuItem,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
   DialogContentText,
 } from '@mui/material';
+import { useHistory } from 'react-router-dom';
 
-const updateContact = ({ contact, onUpdateContact, onClose, open }) => {
+const UpdateContact = ({ contact, onUpdateContact }) => {
+  const history = useHistory();
   const [updatedContact, setUpdatedContact] = useState({});
   const [confirmDialogOpen, setConfirmDialogOpen] = useState(false);
 
@@ -38,88 +39,82 @@ const updateContact = ({ contact, onUpdateContact, onClose, open }) => {
   };
 
   const handleUpdateContact = () => {
-    // Add validation or any other logic before updating
     handleCloseConfirmDialog(); // Close the confirmation dialog
     onUpdateContact(updatedContact);
-    onClose();
+    // Optionally, you can redirect to a different route after updating
+    history.push('/');
   };
 
   return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Update Contact</DialogTitle>
-      <DialogContent>
-        {/* Full Name */}
-        <TextField label="Full Name" value={updatedContact?.fullName || ''} disabled />
+    <div>
+      {/* Full Name */}
+      <TextField label="Full Name" value={updatedContact?.fullName || ''} disabled />
 
-        {/* Email Address */}
-        <TextField
-          label="Email Address"
-          value={updatedContact?.emailAddress || ''}
-          onChange={(e) => handleInputChange('emailAddress', e.target.value)}
-        />
+      {/* Email Address */}
+      <TextField
+        label="Email Address"
+        value={updatedContact?.emailAddress || ''}
+        onChange={(e) => handleInputChange('emailAddress', e.target.value)}
+      />
 
-        {/* Contact Number */}
-        <TextField
-          label="Contact Number"
-          value={updatedContact?.contactNumber || ''}
-          onChange={(e) => handleInputChange('contactNumber', e.target.value)}
-        />
+      {/* Contact Number */}
+      <TextField
+        label="Contact Number"
+        value={updatedContact?.contactNumber || ''}
+        onChange={(e) => handleInputChange('contactNumber', e.target.value)}
+      />
 
-        {/* Location */}
-        <FormControl>
-          <InputLabel>Select Location</InputLabel>
-          <Select
-            value={updatedContact?.location || ''}
-            onChange={(e) => handleInputChange('location', e.target.value)}
-          >
-            <MenuItem value="Manila">Manila</MenuItem>
-            <MenuItem value="Cebu">Cebu</MenuItem>
-          </Select>
-        </FormControl>
+      {/* Location */}
+      <FormControl>
+        <InputLabel>Select Location</InputLabel>
+        <Select
+          value={updatedContact?.location || ''}
+          onChange={(e) => handleInputChange('location', e.target.value)}
+        >
+          <MenuItem value="Manila">Manila</MenuItem>
+          <MenuItem value="Cebu">Cebu</MenuItem>
+        </Select>
+      </FormControl>
 
-        {/* Registered Date */}
-        <TextField
-          label="Registered Date"
-          type="date"
-          value={updatedContact?.registeredDate || ''}
-          onChange={(e) => handleInputChange('registeredDate', e.target.value)}
-          InputLabelProps={{ shrink: true }}
-        />
+      {/* Registered Date */}
+      <TextField
+        label="Registered Date"
+        type="date"
+        value={updatedContact?.registeredDate || ''}
+        onChange={(e) => handleInputChange('registeredDate', e.target.value)}
+        InputLabelProps={{ shrink: true }}
+      />
 
-        {/* Confirmation Dialog */}
-        <Dialog open={confirmDialogOpen} onClose={handleCloseConfirmDialog}>
-          <DialogTitle>Confirm Update</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Please confirm the update to the following:
-              <br />
-              Email Address: {updatedContact?.emailAddress}
-              <br />
-              Contact Number: {updatedContact?.contactNumber}
-              <br />
-              Location: {updatedContact?.location}
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleCloseConfirmDialog} color="primary">
-              No
-            </Button>
-            <Button onClick={handleUpdateContact} color="primary">
-              Yes
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose} color="primary">
-          Cancel
-        </Button>
-        <Button onClick={handleOpenConfirmDialog} color="primary">
-          Update
-        </Button>
-      </DialogActions>
-    </Dialog>
+      {/* Update Button */}
+      <Button variant="contained" color="primary" onClick={handleOpenConfirmDialog}>
+        Update
+      </Button>
+
+      {/* Confirmation Dialog */}
+      <Dialog open={confirmDialogOpen} onClose={handleCloseConfirmDialog}>
+        <DialogTitle>Confirm Update</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please confirm the update to the following:
+            <br />
+            Email Address: {updatedContact?.emailAddress}
+            <br />
+            Contact Number: {updatedContact?.contactNumber}
+            <br />
+            Location: {updatedContact?.location}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseConfirmDialog} color="primary">
+            No
+          </Button>
+          <Button onClick={handleUpdateContact} color="primary">
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 };
 
-export default updateContact;
+export default UpdateContact;
